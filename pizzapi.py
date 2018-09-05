0ad7f150b71534a1f90a3beb5a77ab12bda29a7e
Diadoom <sjors_b@live.nl>
2018-09-05 14:52:38 +0200

    post request toegevoegd

--- pizzapi.py
+++ pizzapi.py
@@ -14,7 +14,7 @@ def getPizza():
     return jsonify({'pizzaDB': pizzaDB})
 
 
-@app.route("/<string:name>", methods=['GET'])
+@app.route("/name/<string:name>", methods=['GET'])
 def getOnePizza(name):
     resultPizza = []
     for pizza in pizzaDB:
@@ -22,6 +22,40 @@ def getOnePizza(name):
             resultPizza.append(pizza)
 
     return jsonify({'pizzaDB':resultPizza})
+
+@app.route("/vorm/<string:vorm>", methods=['GET'])
+def getvormPizza(vorm):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if vorm == pizza['vorm']:
+            resultPizza.append(pizza)
+
+    return jsonify({'pizzaDB':resultPizza})
+
+@app.route("/", methods=["POST"])
+def addONePizza():
+    postdata = request.json
+    newPizza = {}
+    for x in postdata:
+        if x == "name":
+            newPizza["name"] =  request.json['name']
+
+        if x == "rating":
+            newPizza["rating"]=  request.json['rating']
+
+        if x == "vorm":
+            newPizza["vorm"] =  request.json['vorm']
+
+        if x == "prijs":
+            newPizza["prijs"] =  request.json['prijs']
+
+        if x == "ingredienten":
+            newPizza["ingredienten"] =  request.json['ingredienten']
+
+
+    pizzaDB.append(newPizza)
+    return jsonify({'pizzaDB' : pizzaDB})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
